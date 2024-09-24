4ded8df6e63f7c23e723f621cf74904bb6087438
Chiboubnour <chiboubnour32@gmail.com>
2024-09-24 12:43:07 +0200

    Update bitrank.cpp

--- hls/bitrank.cpp
+++ hls/bitrank.cpp
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <ap_int.h>
-
+//bitrankasm_u64_c
 ap_uint<7> popcount_u64(const ap_uint<64> in) {
     ap_uint<7> out = 0;
     for (int i = 0; i < 64; ++i) {
@@ -11,31 +11,11 @@ ap_uint<7> popcount_u64(const ap_uint<64> in) {
     return out;
 }
 
-// Version 1 de bitrank : VICTOR
 ap_uint<64> bitrank(ap_uint<64> _val, const ap_uint<64> pos) {
     const ap_uint<64> val = _val & ((2ULL << pos) - 1);
     return popcount_u64(val);
 }
-
-// Version 2 de bitrank
-//pos dans un entier de 64 bits varie de 0 a 63 => 2œ6 -1
-//bitrank retourne le nbre de bit a 1 dans un entier de 64 bits
-
-ap_uint<7> bitrank(ap_uint<64> _val, const ap_uint<6> pos) {
-    const ap_uint<64> val = _val & ((2ULL << pos) - 1);
-    return popcount_u64(val);
-}
-
-// Version 3 de bitrank
- ap_uint<7> bitrank(ap_uint<64> _val, const ap_uint<6> pos) {
-    ap_uint<7> count = 0;
-    for (int i = 0; i < pos; ++i) {
-        #pragma HLS UNROLL factor=3
-        count += _val[i];
-    }
-    return count;
-}
- // Version 4 de bitrank
+//bitrank_u64
 ap_uint<7> bitrank (ap_uint<64> _val, const ap_uint<6> pos) {
      ap_uint<7> count = 0;
      for (int i = 0; i < 64; ++i) {
